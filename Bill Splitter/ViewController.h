5cb14456415ebf3f6357a512a42d5379ae456cd3
escakot <ec@neorennie.com>
2017-07-09 10:11:29 -0400

    Initial Commit

---
+++ Bill Splitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bill Splitter
+//
+//  Created by Errol Cheong on 2017-07-09.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
