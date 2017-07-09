19eed7711b3ea507847c8a38b1405f4ef4af9948
escakot <ec@neorennie.com>
2017-07-09 16:34:44 -0400

    Added function
    Calculates bill split between 2-8 people
    Added tip calculation
    No button press required, calculation occurs
    after any changes are made

--- Bill Splitter/ViewController.m
+++ Bill Splitter/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UISlider *numberOfPeopleSplitting;
+@property (weak, nonatomic) IBOutlet UISlider *tipPercentageSlider;
+@property (weak, nonatomic) IBOutlet UILabel *splitBillAmount;
+@property (weak, nonatomic) IBOutlet UILabel *tipPercentageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numberOfPeopleSplittingLabel;
 
 @end
 
@@ -17,6 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.billAmountTextField.delegate = self;
 }
 
 
@@ -25,5 +32,41 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)textFieldDidEndEditing:(UITextField *)textField{
+    [self calculateSplitBill];
+    [textField resignFirstResponder];
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [self calculateSplitBill];
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)calculateSplitBill{
+    float percentageTip = 1 + (self.tipPercentageSlider.value / 100);
+    float totalBill = ([self.billAmountTextField.text floatValue] * percentageTip)/self.numberOfPeopleSplitting.value;
+    NSNumber *splitBill = [NSDecimalNumber numberWithFloat:totalBill];
+    NSString *splitBillText = [NSNumberFormatter localizedStringFromNumber:splitBill numberStyle:NSNumberFormatterCurrencyStyle];
+    self.splitBillAmount.text = splitBillText;
+}
+- (IBAction)splitBillSlider:(UISlider *)sender {
+    NSInteger sliderValue = lroundf(sender.value);
+    [sender setValue:sliderValue animated:YES];
+    self.numberOfPeopleSplittingLabel.text =
+    [NSString stringWithFormat:@"Number of People Splitting: %li", sliderValue];
+    [self calculateSplitBill];
+}
+- (IBAction)tipPercentageSlider:(UISlider *)sender {
+    NSInteger sliderValue = lroundf(sender.value);
+    [sender setValue:sliderValue animated:YES];
+    self.tipPercentageLabel.text =
+    [NSString stringWithFormat:@"Percentage Tip: %li%%", sliderValue];
+    [self calculateSplitBill];
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
 
 @end
